6c85d45ea63a867406c91f3a22e8de92dfecd5bd
Madhumidha14 <65933615+Madhumidha14@users.noreply.github.com>
2021-08-19 11:13:12 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os #os is a python utility module
+prime = os.path.splitext('abc.txt')#splitext()is used to split root and ext 
+file_name = prime[0]
+file_extension =prime[1]
+print("file_name=",file_name)
+print("file_extension=",file_extension)
+    
+    
+    
+    
+
+      
+      
