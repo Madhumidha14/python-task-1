3aa13d0e081b863da063307bb8b04549823bd1fe
Madhumidha14 <65933615+Madhumidha14@users.noreply.github.com>
2021-08-20 10:36:12 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 19 11:00:31 2021
+
+@author: DELL
+"""
+
+import math
+r = float(input("enter the radius of circle:"))
+area = math.pi*r*r
+print(area)
